f8363e253c9eba3dfa8f2cfe3ad9cd818220d2bd
pavan19a97 <47987982+pavan19a97@users.noreply.github.com>
2019-10-26 19:59:01 +0530

    Add files via upload

--- Bingoclient1.java
+++ Bingoclient1.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.LongBuffer;
+import java.util.Scanner;
 
 public class Bingoclient1 {
 
@@ -14,14 +15,16 @@ public static void main(String args[]) throws IOException{
     InetAddress address=InetAddress.getLocalHost();
     Socket s1=null;
     String line=null;
-    BufferedReader br=null;
+    Scanner br = null,sr=null;
+    br=null;
     //LongBuffer br = LongBuffer.allocate(64);
     BufferedReader is=null;
     PrintWriter os=null;
 
     try {
         s1=new Socket(address, 58901); // You can use static final constant PORT_NUM
-        br= new BufferedReader(new InputStreamReader(System.in));
+        br= new Scanner(new InputStreamReader(System.in));
+        sr= new Scanner(new InputStreamReader(System.in));
         is=new BufferedReader(new InputStreamReader(s1.getInputStream()));
         os= new PrintWriter(s1.getOutputStream());
     }
@@ -42,14 +45,28 @@ public static void main(String args[]) throws IOException{
         System.out.println(response);
         response = is.readLine();
             System.out.println(response);
+        System.out.println("Fill in values 1 to 25");
+        int[][] mat=new int[3][3];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                mat[i][j] = br.nextInt();
+            }
+        }
+        System.out.println("here :"+mat[1][1]);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
         if(mark == 'f'){
-            line = br.readLine();
+            line = sr.nextLine();
             os.println(line);os.flush();
         }
         while(true){
             response = is.readLine();
             System.out.println(response);
-            line = br.readLine();
+            line = sr.nextLine();
             os.println(line);os.flush();
         }
         
